3aaf6631fca6623fba2e0a24a29cf3d5de49dd54
knocken <knocken@126.com>
2014-12-27 15:02:10 +0800

    test commit

--- app/src/main/java/com/example/zhongming/myapplication/MainActivity.java
+++ app/src/main/java/com/example/zhongming/myapplication/MainActivity.java
@@ -4,16 +4,32 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button mBtn = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
     }
 
+    private void initViews() {
+        mBtn = (Button) findViewById(R.id.button);
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "good", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
